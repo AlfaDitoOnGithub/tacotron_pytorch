c0f7cfd1815ee8ef9f153c3c00806acd96ed14b1
AlfaDitoOnGithub <alfaditoauliadenova@gmail.com>
2023-10-16 13:20:56 +0700

    changes in hparam

--- hparams.py
+++ hparams.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
+import sys
+sys.path.insert(0, '/')
 
+import hparamBASED as Hparam
+
+# import hparamBASED as hparam
+# ini doang yg trouble
 
 # Default hyperparameters:
-hparams = tf.contrib.training.HParams(
+hparams = Hparam.HParams(
     # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
     # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
     cleaners='english_cleaners',
